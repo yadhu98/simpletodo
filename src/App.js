a1a119b70c06b43286f20f765bd31c1936460e7c
YADHU KRISHNAN <yadhu98@gmail.com>
2025-03-18 14:09:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import TodoMain from "./component/TodoMain";
 import "./styles.css";
+// import { Provider } from "react-redux";
+// import { store } from "./store";
 
 export default function App() {
   return (
     <div className="App">
+      {/* <Provider store={store}> */}
       <TodoMain />
+      {/* </Provider> */}
     </div>
   );
 }
